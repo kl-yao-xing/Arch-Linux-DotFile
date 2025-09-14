bf56727b009f1a210047f7f15dd6a125daf5b870
kl-yao-xing <baokaizhe@gmail.com>
2025-09-14 17:47:45 +0800

    init alias

---
+++ .config/bash/alias.sh
@@ -0,0 +1,12 @@
+#
+# ~/.config/bash/alias.sh
+#
+# some alias
+
+alias ls='ls -a --color=auto'
+alias tree='tree -a'
+alias grep='grep --color=auto'
+alias river='exec river'
+alias vim='nvim'
+alias pacman='sudo pacman'
+alias wget="wget --hsts-file="$HOME/.cache/wget/wget-hsts""
