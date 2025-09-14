3bf78315afb8c1694e933e749bb45da0cad9ebc4
kl-yao-xing <baokaizhe@gmail.com>
2025-09-14 17:51:21 +0800

    init profile.sh

---
+++ .config/bash/profile.sh
@@ -0,0 +1,28 @@
+#
+# ~/.config/bash/profile.sh
+# 
+# some envs
+
+# envs
+export EDITOR=nvim
+export LANG=zh_CN.UTF-8
+export LANGUAGE=zh_CN:en_US 
+
+# settings
+# bash
+## history file
+export HISTFILE="$HOME/.cache/bash/bash_history"
+## history ignore
+export HISTIGNORE=exit:poweroff:river
+export HISTCONTROL=ignoredups
+
+# less
+## history file
+export LESSHISTFILE="$HOME/.cache/less/lesshst"
+
+# XDG_FILES
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# pluseaudio
+export PULSE_COOKIE="$HOME/.config/pulse/cookie"
